645c5e9c4c3cf8e930587b280dbc41b8f9c7442b
Rashmi <rashmi.rrp@gmail.com>
2023-01-21 10:00:15 +0530

    first commit

---
+++ src/main/java/com/java4you/DevopsIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.java4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+		System.out.println("Hi all");
+	}
+
+}
